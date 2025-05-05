0d4d07152253be3895a28b7f49f3f13163255d9e
joshuazhong1 <jozhong@hmc.edu>
2025-05-04 21:01:37 -0700

    Fix intro part 4

--- src/Timer.js
+++ src/Timer.js
@@ -13,6 +13,7 @@ const LOSE_DELAY = 17000;
 const TIMER_SECS = 2700; // 2700 = 45:00
 const FIREBASE_COLLECTION = "timers";
 const FIREBASE_DOC = "timer1";
+var has_lost = false;
 
 function formatMsecs(msecs) {
   // format time in MM:SS
@@ -59,9 +60,7 @@ function Timer({ db }) {
   };
 
   const onStart = () => {
-    updateDoc(doc(db, FIREBASE_COLLECTION, FIREBASE_DOC), {
-      intro: true
-    });
+    timer.intro = true;
 
     content = (
       <TimerContents loading={loading} formattedTime={formattedTime} />
@@ -89,27 +88,32 @@ function Timer({ db }) {
       return;
     }
 
+    // updateDoc(doc(db, FIREBASE_COLLECTION, FIREBASE_DOC), {
+    //   secs: getRemainingMsecs() / 1000,
+    //   startTime: null,
+    // });
+
+    setTimeout(function() {
+      updateDoc(doc(db, FIREBASE_COLLECTION, FIREBASE_DOC), {
+        credits: true
+      });
+    }, WIN_DELAY);
+
     updateDoc(doc(db, FIREBASE_COLLECTION, FIREBASE_DOC), {
         secs: getRemainingMsecs() / 1000,
         startTime: null,
         win: true,
     });
-
-    setTimeout(function() {
-      updateDoc(doc(db, FIREBASE_COLLECTION, FIREBASE_DOC), {
-        wincredits: true,
-        win: false
-      });
-    }, WIN_DELAY);
   };
 
   const onReset = () => {
+    has_lost = false;
+
     updateDoc(doc(db, FIREBASE_COLLECTION, FIREBASE_DOC), {
       secs: TIMER_SECS,
       startTime: null,
       win: false,
-      lose: false,
-      wincredits: false,
+      credits: false,
       losecredits: false,
       intro: false,
     });
@@ -130,6 +134,8 @@ function Timer({ db }) {
     formattedTime = formatMsecs(getRemainingMsecs());
   }
 
+  console.log("timer screen");
+
   var content = (
     <>
       <ReactAudioPlayer
@@ -144,66 +150,50 @@ function Timer({ db }) {
     </>
   );
 
-  if (getRemainingMsecs() <= 0) {
-    updateDoc(doc(db, FIREBASE_COLLECTION, FIREBASE_DOC), {
-      lose: true
-    });
+  if (timer?.credits) {
+    content = (
+      <WinScreen finishedIn={formatMsecs(TIMER_SECS * 1000 - getRemainingMsecs())} />
+    );
+  } else if (timer?.win) {
+    content = (
+      <>
+        <ReactAudioPlayer
+          src="winaudio.mp3"
+          volume = {0.1}
+          ref={(e) => {
+            bgAudioRef.current = e;
+          }}
+        />
+        <TimerContents loading={loading} formattedTime={formattedTime} />
+      </>
+    );
+  } else if (timer?.lose) {
+    has_lost = true;
+    new Audio("loseaudio.mp3").play();
 
     setTimeout(function() {
       updateDoc(doc(db, FIREBASE_COLLECTION, FIREBASE_DOC), {
-        losecredits: true,
-        lose: false
+        losecredits: true
       });
     }, LOSE_DELAY);
+  } else if (timer?.intro) {
+    content = (
+      <>
+        <ReactAudioPlayer
+          src="introtwist.mp3"
+          ref={(e) => {
+            bgAudioRef.current = e;
+          }}
+        />
+        <TimerContents loading={loading} formattedTime={formattedTime} />
+      </>
+    );
   }
 
   if (timer?.losecredits) {
     content = (
       <LoseScreen finishedIn={formatMsecs(TIMER_SECS * 1000 - getRemainingMsecs())} />
     );
-  } else if (timer?.wincredits) {
-    content = (
-      <WinScreen finishedIn={formatMsecs(TIMER_SECS * 1000 - getRemainingMsecs())} />
-    );
-  } else if (timer?.intro) {
-    content = (
-      <>
-        <ReactAudioPlayer
-          src="introtwist.mp3"
-          loop
-          ref={(e) => {
-            bgAudioRef.current = e;
-          }}
-        />
-        <TimerContents loading={loading} formattedTime={formattedTime} />
-      </>
-    )
-  } else if (timer?.win) {
-    content = (
-      <>
-        <ReactAudioPlayer
-          src="winaudio.mp3"
-          loop
-          ref={(e) => {
-            bgAudioRef.current = e;
-          }}
-        />
-        <TimerContents loading={loading} formattedTime={formattedTime} />
-      </>
-    )
-  } else if (timer?.lose) {
-    content = (
-      <>
-        <ReactAudioPlayer
-          src="loseaudio.mp3"
-          loop
-          ref={(e) => {
-            bgAudioRef.current = e;
-          }}
-        />
-        <TimerContents loading={loading} formattedTime={formattedTime} />
-      </>
-    )
   }
 
   return (
